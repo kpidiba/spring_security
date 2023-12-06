e66cea8641df190543b366ca38b6712df4128ca8
dprogrammer <kpidibadavid1@gmail.comm>
2023-12-06 09:39:35 +0000

    delete unused  library

--- test/src/main/java/com/security/test/controllers/AuthController.java
+++ test/src/main/java/com/security/test/controllers/AuthController.java
@@ -3,8 +3,6 @@ package com.security.test.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.security.test.config.security.AuthenticationRequest;
 import com.security.test.config.security.AuthenticationResponse;
-import com.security.test.config.security.JwtService;
 import com.security.test.config.security.RegisterRequest;
 import com.security.test.services.AuthenticationService;
 
--- test/src/main/java/com/security/test/models/User.java
+++ test/src/main/java/com/security/test/models/User.java
@@ -30,7 +30,6 @@ public class User implements UserDetails{
 
     @Enumerated(EnumType.STRING)
     private Role role;
-
     
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
