c3fd867b5b463a500873b8eec54f10f6fc81ce7a
kbrightcoder <kpidibadavid1@gmail.com>
2024-06-10 19:28:07 +0000

    auto create -init user

--- security/src/main/java/com/security/security/SecurityApplication.java
+++ security/src/main/java/com/security/security/SecurityApplication.java
@@ -1,11 +1,25 @@
 package com.security.security;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.security.security.models.request.RegisterRequest;
+import com.security.security.repository.UserRepository;
+import com.security.security.services.AuthenticationService;
 
 @SpringBootApplication
 public class SecurityApplication {
 
+	@Bean
+	CommandLineRunner init(UserRepository repository,AuthenticationService service) {
+		return args -> {
+			if (!repository.existsUserByUsername("admin")) {
+				service.register(new RegisterRequest("admin","password","ADMIN"));
+			}
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
